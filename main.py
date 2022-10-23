3cc5c062c2acf0b5e23c30fbf0c4defb04754c01
Its-MatriX <109545908+Its-MatriX@users.noreply.github.com>
2022-10-23 20:00:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,63 @@
-from keyboard import add_hotkey, remove_hotkey, write
+from keyboard import add_hotkey, remove_hotkey
+from time import sleep
+from keyboard import write as _write
 from time import sleep
 from playsound import playsound
 from os.path import split
-from colorama import Fore
+from random import choice, randint
+from os import _exit
+from ctypes import windll
+from threading import Thread
+
+Thread(target=lambda:windll.user32.MessageBoxW(0, 'Ctrl+Shift+S - Пауза/Продолжить\n' + \
+                             'Ctrl+Shift+E - Закрыть', 'ScaryType', 0x40 | 0x10000)).start()
 
 Folder = split(__file__)[0].replace('\\', '/')
 
 State = True
 
+
+def CurseLetter(Letter):
+    CursedChars = (769, 771, 772, 773, 774, 775, 776, 777, 778, 782, 783, 785,
+                   786, 789, 791, 793, 794, 796, 799, 800, 801, 802, 803, 804,
+                   805, 807, 808, 810, 812, 816, 817, 818, 819, 820, 821, 822,
+                   823, 824, 826, 829, 830, 832, 833, 835, 836, 837, 839, 841,
+                   842, 844, 846, 850, 852, 853, 854, 855, 857, 859, 860, 861,
+                   864)
+
+    ToReturn = ''
+
+    for x in range(randint(0, 3)):
+        ToReturn += chr(choice(CursedChars))
+
+    ToReturn += Letter
+    return ToReturn
+
+
+def WriteLetter(Letter):
+    _write(CurseLetter(Letter))
+
+
 def AddHotkeys():
-    add_hotkey(',', callback=lambda: write('6'), suppress=True)
-    add_hotkey('c', callback=lambda: write('s'), suppress=True)
-    add_hotkey('p', callback=lambda: write('z'), suppress=True)
-    add_hotkey('x', callback=lambda: write('4'), suppress=True)
-    add_hotkey('b', callback=lambda: write('u'), suppress=True)
-    add_hotkey('g', callback=lambda: write('n'), suppress=True)
-    add_hotkey('d', callback=lambda: write('v'), suppress=True)
-    add_hotkey('n', callback=lambda: write('t'), suppress=True)
-    add_hotkey('q', callback=lambda: write('j'), suppress=True)
-    add_hotkey('l', callback=lambda: write('d'), suppress=True)
-    add_hotkey('r', callback=lambda: write('k'), suppress=True)
-    add_hotkey('j', callback=lambda: write('0'), suppress=True)
+    add_hotkey(',', callback=lambda: WriteLetter('6'), suppress=True)
+    add_hotkey('c', callback=lambda: WriteLetter('s'), suppress=True)
+    add_hotkey('p', callback=lambda: WriteLetter('z'), suppress=True)
+    add_hotkey('x', callback=lambda: WriteLetter('4'), suppress=True)
+    add_hotkey('b', callback=lambda: WriteLetter('u'), suppress=True)
+    add_hotkey('g', callback=lambda: WriteLetter('n'), suppress=True)
+    add_hotkey('d', callback=lambda: WriteLetter('v'), suppress=True)
+    add_hotkey('n', callback=lambda: WriteLetter('t'), suppress=True)
+    add_hotkey('q', callback=lambda: WriteLetter('j'), suppress=True)
+    add_hotkey('l', callback=lambda: WriteLetter('d'), suppress=True)
+    add_hotkey('r', callback=lambda: WriteLetter('k'), suppress=True)
+    add_hotkey('j', callback=lambda: WriteLetter('0'), suppress=True)
 
     try:
-        add_hotkey('ё', callback=lambda: write('ё'), suppress=True)
+        add_hotkey('ё', callback=lambda: WriteLetter('ё'), suppress=True)
 
     except:
-        add_hotkey('~', callback=lambda: write('ё'), suppress=True)
+        add_hotkey('~', callback=lambda: WriteLetter('ё'), suppress=True)
+
 
 def RemoveHotkeys():
     remove_hotkey(',')
@@ -48,6 +79,7 @@ def RemoveHotkeys():
     except:
         remove_hotkey('~')
 
+
 def ChangeState():
     global State
 
@@ -55,18 +87,19 @@ def ChangeState():
 
     if not State:
         RemoveHotkeys()
-        print(Fore.RED + 'Поставлено на паузу')
         playsound(Folder + '/state_changed.mp3')
 
     else:
-        print(Fore.CYAN + 'Продолжение работы')
         playsound(Folder + '/state_changed.mp3')
         AddHotkeys()
 
+
 AddHotkeys()
 
-add_hotkey('Ctrl+Shift+S', ChangeState)
-print(Fore.GREEN + 'Программа запущена!')
+add_hotkey('Ctrl+Shift+S', ChangeState, suppress=True)
+add_hotkey('Ctrl+Shift+E',
+           lambda: playsound(Folder + '/state_changed.mp3') | _exit(0),
+           suppress=True)
 
 playsound(Folder + '/state_changed.mp3')
 
